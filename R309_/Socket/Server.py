f3e2f68729868dee6cb2a46f5570d3ea5774a282
JLKayser <92865016+JLKayser@users.noreply.github.com>
2022-11-18 08:45:02 +0100

    Update Server.py

--- R309_/Socket/Server.py
+++ R309_/Socket/Server.py
@@ -1,26 +1,25 @@
-#Synchrone
 import socket
 
 
 def server():
-    # Recevoir me hostname
+    # Recevoir le hostname soit ip de la machine
     host = socket.gethostname()
     port = 5000  # Port
 
     server_socket = socket.socket()  # get instance
     server_socket.bind((host, port))
 
-    # nombre de client qui peuvent de se connecter en meme temp
+    # nombre de client qui peuvent se connecter en meme temp
     server_socket.listen(2)
     conn, address = server_socket.accept()  # accepte les nouvelles connexions
-    print("Une connexion a été détecté: " + str(address))
+    print("Connection from: " + str(address))
     while True:
         # recois les données et n accepte pas au dessus de 1024 bytes
         data = conn.recv(1024).decode()
         if not data:
             # Si les données ne sont pas recus break
             break
-        print("de l'utilisateur connecté: " + str(data))
+        print("from connected user: " + str(data))
         data = input(' -> ')
         conn.send(data.encode())  # Envoye les données au client
 
