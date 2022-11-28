b37d0573918a118c6dd8e8c3669e386d1b7a0bc1
JLKayser <92865016+JLKayser@users.noreply.github.com>
2022-11-28 16:45:33 +0100

    Update Server.py

--- R309_/Socket/Server.py
+++ R309_/Socket/Server.py
@@ -1,88 +1,33 @@
-import socket, threading
-import sys , os , subprocess
+import socket
 
 
-def ipconfig():
-    cmd = str(subprocess.check_output('ipconfig',shell=True))
-    x = cmd.replace('\\r',"").replace('\\b','').replace('\\n','\n').replace('\\xff','').replace("'",'')
-    return x
+def server():
+    # Recevoir le hostname soit ip de la machine
+    host = "localhost"
+    port = 5000  # Port
+    server_socket = socket.socket()  # get instance
+    server_socket.bind((host, port))
 
 
-def hostname():
-    cmd = str(subprocess.check_output('hostname',shell=True))
-    x = cmd.replace('\\r', "").replace('\\b', '').replace('\\n', '\n').replace('\\xff', '').replace('b',"").replace("'",'')
-    return x
+    # nombre de client qui peuvent se connecter en meme temp
+    server_socket.listen(1)
+
+    conn, address = server_socket.accept()  # accepte les nouvelles connexions
+    print("Connection from: " + str(address))
+    while True:
+        # recois les données et n accepte pas au dessus de 1024 bytes
+        data = conn.recv(1024).decode()
+        if not data:
+            # Si les données ne sont pas recus break
+            server_socket.close()
+            server()
+            break
+        print("from connected user: " + str(data))
+        data = input(' -> ')
+        conn.send(data.encode())  # Envoyer les données au client
+
+    conn.close()  # Fermer la connexion
 
 
-def os_command():
-    cmd = sys.platform
-    if cmd == 'win32':
-        cmd = 'OS de la machine est Windows 10'
-    x = cmd.replace('\\r', "").replace('\\b', '').replace('\\n', '\n').replace('\\xff', '').replace("'",'')
-    return x
-
-def ram():
-    cmd = str(subprocess.check_output('wmic memphysical get MaxCapacity', shell=True))
-    x = cmd.replace('\\r', "").replace('\\b', '').replace('\\n', '').replace('\\xff', '').replace('b', "").replace("'",'')
-    return f'{x} KB'
-
-
-'''def reset():
-    x = sys.argv[0]
-    cmd = os.system(x + ' py')
-    return cmd'''
-
-
-class ClientThread(threading.Thread):
-    def __init__(self,clientAddress,clientsocket):
-        threading.Thread.__init__(self)
-        self.csocket = clientsocket
-        print ("New connection added: ", clientAddress)
-
-
-    def run(self):
-        print ("Connection from : ", clientAddress)
-        #self.csocket.send(bytes("Hi, This is from Server..",'utf-8'))
-        while True:
-            try:
-                data = self.csocket.recv(2048)
-                if data is not None:
-                    msg = data.decode()
-                    if msg == 'disconnect':
-                      break
-                    if msg == 'kill':
-                        server.close()
-                        break
-                    '''if msg=='reset':
-                        reset()
-                        break'''
-                    if msg == 'ip':
-                        self.csocket.send(ipconfig().encode())
-                    if msg == 'hostname':
-                        self.csocket.send(hostname().encode())
-                    if msg == 'os':
-                        self.csocket.send(os_command().encode())
-                    if msg == 'ram':
-                        self.csocket.send(ram().encode())
-                    print ("from client", msg)
-            except:
-                print("Client disconnected")
-                self.csocket.close()
-                break
-        print ("Client at ", clientAddress, " disconnected...")
-
-
-PORT = 5500
-ADDRESS = '127.0.0.1'
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind((ADDRESS, PORT))
-
-msg = ''
-print("Server started")
-print("Waiting for client request..")
-while True:
-    server.listen(1)
-    clientsock, clientAddress = server.accept()
-    newthread = ClientThread(clientAddress, clientsock)
-    newthread.start()
+if __name__ == '__main__':
+    server()
